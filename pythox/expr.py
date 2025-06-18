5b2658538fd5769871e97622acb769d7b6d7a8de
serdonke <github@serdonke.xyz>
2025-06-19 04:43:38 +0530

    meta-programming bby

---
+++ pythox/expr.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+from ttoken import Token
+
+class Expr:
+    pass
+
+@dataclass(frozen=True, slots=True)
+class Assign(Expr):
+    name: Token
+    value: Expr
+
+@dataclass(frozen=True, slots=True)
+class Binary(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+@dataclass(frozen=True, slots=True)
+class Grouping(Expr):
+    expression: Expr
+
+@dataclass(frozen=True, slots=True)
+class Literal(Expr):
+    # Hmmm should we use object?
+    value: float | str | bool | None
+
+@dataclass(frozen=True, slots=True)
+class Logical(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+@dataclass(frozen=True, slots=True)
+class Unary(Expr):
+    operator: Token
+    right: Expr
+
+@dataclass(frozen=True, slots=True)
+class Ternary(Expr):
+    left: Expr
+    qmark: Token
+    middle: Expr
+    colon: Token
+    right: Expr
+
+@dataclass(frozen=True, slots=True)
+class Variable(Expr):
+    name: Token
+
---
+++ tools/generateAst.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import sys
+
+def main():
+    outputDir = Path(__file__).resolve().parent.parent / "pythox"
+    if outputDir.exists() and outputDir.is_dir():
+        print(f"\033[32mOutputting in {outputDir}\033[0m")
+    else:
+        outputDir = Path(__file__).resolve().parent
+        print(f"\033[31mOutputting to tools dir\033[0m", file=sys.stderr)
+    
+    expr: dict = {"Assign"   : "Token name, Expr value",
+                  "Binary"   : "Expr left, Token operator, Expr right",
+                  "Grouping" : "Expr expression",
+                  "Literal"  : "object value",
+                  "Logical"  : "Expr left, Token operator, Expr right",
+                  "Unary"    : "Token operator, Expr right",
+                  "Ternary"  : "Expr left, Token qmark, Expr middle, Token colon, Expr right",
+                  "Variable" : "Token name"}
+    defineAst(outputDir, "expr", expr)
+
+def defineAst(outputDir: Path, baseName: str, types: dict[str, str]):
+    path = outputDir / f"{baseName}.py"
+    with open(path, "w") as f:
+        f.write("from dataclasses import dataclass\n\n")
+        f.write("from .token import Token\n\n")
+
+        f.write(f"class {baseName.capitalize()}:\n")
+        f.write("    pass\n\n")
+
+        for className, fieldList in types.items():
+            fields = [field.strip() for field in fieldList.split(',')]
+            f.write(f"@dataclass(frozen=True, slots=True)\n")
+            f.write(f"class {className}({baseName.capitalize()}):\n")
+            for field in fields:
+                tType, name = field.split(' ')
+                if tType == "object":
+                     f.write(f"    # Hmmm should we use {tType}?\n")
+                     # Doing something similar in Token Class
+                     # Kinda follows the spec methinks
+                     f.write(f"    {name}: float | str | bool | None\n")
+                     continue
+
+                f.write(f"    {name}: {tType}\n")
+            f.write("\n")
+
+        print(f"\033[34mWrote {path.name}!\033[0m")
+
+if __name__ == "__main__":
+    main()
